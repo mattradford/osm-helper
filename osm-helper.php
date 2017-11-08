2db37fca0302fb0a24230fc2c81d6affe085c8e5
Matt Radford <webform@mattrad.co.uk>
2017-11-08 23:14:28 +0000

    Reset version.

--- osm-helper.php
+++ osm-helper.php
@@ -8,7 +8,7 @@
  * Author URI:  https://www.mattrad.uk
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain: osm-helper
+ * Text Domain: osmhelper
  * Domain Path: /languages
  */
 
